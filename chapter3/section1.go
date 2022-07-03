9bfc9c8338332911b4a83b47e762071913c1377a
neefrankie <neefrankie@gmail.com>
2022-07-03 23:07:53 +0800

    init

---
+++ chapter3/section1.go
@@ -0,0 +1,49 @@
+package chapter3
+
+import (
+	"fmt"
+	"sync"
+)
+
+func SayHello() {
+	var wg sync.WaitGroup
+	sayHello := func() {
+		defer wg.Done()
+		fmt.Println("hello")
+	}
+
+	wg.Add(1)
+	go sayHello()
+
+	wg.Wait()
+}
+
+func Salutation() {
+	var wg sync.WaitGroup
+
+	salutation := "hello"
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		salutation = "welcome"
+	}()
+
+	wg.Wait()
+
+	fmt.Println(salutation)
+}
+
+func Salutations() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+
+	wg.Wait()
+}
---
+++ chapter3/section1_test.go
@@ -0,0 +1,48 @@
+package chapter3
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test SayHello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SayHello()
+		})
+	}
+}
+
+func TestSalutation(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test Salutation",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Salutation()
+		})
+	}
+}
+
+func TestSalutations(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Captured value",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Salutations()
+		})
+	}
+}
