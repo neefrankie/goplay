813469c384e9e94217d3d307173ba4219baa369b
neefrankie <neefrankie@gmail.com>
2022-07-31 23:55:35 +0800

    test

---
+++ chapter3/section2_test.go
@@ -0,0 +1,63 @@
+package chapter3
+
+import "testing"
+
+func TestWaitComplete(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Wait to complete",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			WaitComplete()
+		})
+	}
+}
+
+func TestWaitCompleteN(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Wait an array of goroutines",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			WaitCompleteN()
+		})
+	}
+}
+
+func TestMutualExclusion(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test Mutex",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			MutualExclusion()
+		})
+	}
+}
+
+func TestReadWriteMutex(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test Read Write Mutex",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReadWriteMutex()
+		})
+	}
+}
