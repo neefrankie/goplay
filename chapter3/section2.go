13b6bd7b07e49e6235d755abd38b440578a648bc
neefrankie <neefrankie@gmail.com>
2022-07-31 23:55:28 +0800

    sync package

---
+++ chapter3/section2.go
@@ -0,0 +1,137 @@
+package chapter3
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"sync"
+	"text/tabwriter"
+	"time"
+)
+
+func WaitComplete() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("1st goroutine sleeping...")
+		time.Sleep(1)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("2nd goroutine sleeping...")
+		time.Sleep(2)
+	}()
+
+	wg.Wait()
+	fmt.Println("All goroutines complete")
+}
+
+func WaitCompleteN() {
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %d!\n", id)
+	}
+
+	const numbGreeters = 5
+
+	var wg sync.WaitGroup
+	wg.Add(numbGreeters)
+
+	for i := 0; i < numbGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
+}
+
+func MutualExclusion() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrementing: %d\n", count)
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			increment()
+		}()
+	}
+
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			decrement()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Arithmetic complete.")
+}
+
+func ReadWriteMutex() {
+	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		for i := 5; i > 0; i-- {
+			l.Lock()
+			l.Unlock()
+			time.Sleep(1)
+		}
+	}
+
+	observer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		l.Lock()
+		defer l.Unlock()
+	}
+
+	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+		wg.Add(count + 1)
+
+		beginTestTime := time.Now()
+
+		go producer(&wg, mutex)
+
+		for i := count; i > 0; i-- {
+			go observer(&wg, rwMutex)
+		}
+
+		wg.Wait()
+		return time.Since(beginTestTime)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutex\tMutex\n")
+
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+		fmt.Fprintf(
+			tw,
+			"%d\t%v\t%v\n",
+			count,
+			test(count, &m, m.RLocker()),
+			test(count, &m, &m))
+	}
+}
