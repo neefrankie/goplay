cb51ac33936dbbaa6a136cf79d2ebc70ca33510c
neefrankie <neefrankie@gmail.com>
2022-08-10 23:05:57 +0800

    toInt

--- concurrency/patterns/pipelines.go
+++ concurrency/patterns/pipelines.go
@@ -120,3 +120,18 @@ func toString(done <-chan interface{}, valueStream <-chan interface{}) <-chan st
 
 	return stringStream
 }
+
+func toInt(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for v := range valueStream {
+			select {
+			case <-done:
+				return
+			case intStream <- v.(int):
+			}
+		}
+	}()
+	return intStream
+}
