15937583e6bde8952d43eb28342fcb59a0392379
weiguo.ni <neefrankie@gmail.com>
2022-09-30 16:26:53 +0800

    format any value

---
+++ gopl/ch12/format_test.go
@@ -0,0 +1,15 @@
+package ch12
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAny(t *testing.T) {
+	var x int64 = 1
+	var d time.Duration = 1 * time.Nanosecond
+	t.Log(Any(x))
+	t.Log(Any(d))
+	t.Log(Any([]int64{x}))
+	t.Log(Any([]time.Duration{d}))
+}
