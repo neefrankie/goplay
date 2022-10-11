a5334adc35a1845d5dbc964fb8addd37264a2b6e
weiguo.ni <neefrankie@gmail.com>
2022-10-11 17:15:40 +0800

    flag

---
+++ gopl/ch7/celsius_flag.go
@@ -0,0 +1,35 @@
+package ch7
+
+import (
+	"flag"
+	"fmt"
+	"goplay/gopl/ch2"
+)
+
+type celsiusFlag struct {
+	ch2.Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Scanf(s, "%f%s", &value, &unit)
+
+	switch unit {
+	case "C", "°C":
+		f.Celsius = ch2.Celsius(value)
+		return nil
+
+	case "F", "°F":
+		f.Celsius = ch2.FToC(ch2.Fahrenheit(value))
+		return nil
+	}
+
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value ch2.Celsius, usage string) *ch2.Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
---
+++ gopl/ch7/tempflag/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"goplay/gopl/ch7"
+)
+
+var temp = ch7.CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
