aea4abc110de7aedcf8613e59ec9826e67bf0838
weiguo.ni <neefrankie@gmail.com>
2022-10-13 15:39:25 +0800

    concurrent crawler

---
+++ gopl/ch8/crawl/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"goplay/gopl/ch5/links"
+	"log"
+	"os"
+)
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+
+	// Acquire a token
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	// Release the token
+	<-tokens
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	var n int // number of pending sends to worklist
+
+	// Start with teh command-line arguments
+	n++
+	go func() {
+		worklist <- os.Args[1:]
+	}()
+
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		for list := range worklist {
+			for _, link := range list {
+				if !seen[link] {
+					seen[link] = true
+					n++
+					go func(link string) {
+						worklist <- crawl(link)
+					}(link)
+				}
+			}
+		}
+	}
+}
