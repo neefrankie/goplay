f8bf8621ff4d9980bd3dae9186652046d5c3150c
weiguo.ni <neefrankie@gmail.com>
2023-06-26 17:04:05 +0800

    test

--- dbdemo/create_test.go
+++ dbdemo/create_test.go
@@ -1,10 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestCreateRecord(t *testing.T) {
 	myDB := mustConnect()
 
 	myDB.AutoMigrate(&User{})
 
+	user := User{
+		Name:     "Jinzhu",
+		Age:      18,
+		Birthday: time.Now(),
+	}
+
+	result := myDB.Create(&user)
+
+	if result.Error != nil {
+		t.Fatal(result.Error)
+	}
+	t.Logf("ID %d, rows affected %d", user.ID, result.RowsAffected)
+
+	users := []*User{
+		{
+			Name:     "Jinzhu",
+			Age:      18,
+			Birthday: time.Now(),
+		},
+		{
+			Name:     "Jackson",
+			Age:      19,
+			Birthday: time.Now(),
+		},
+	}
+
+	result = myDB.Create(users)
+
+	if result.Error != nil {
+		t.Fatal(result.Error)
+	}
+
+	t.Logf("Rows affected %d", result.RowsAffected)
 }
