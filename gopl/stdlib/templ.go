180a7fad981e98030c938b441a512b91fc85731b
neefrankie <neefrankie@gmail.com>
2023-12-20 22:40:47 +0800

    add Renderer

--- gopl/stdlib/templ.go
+++ gopl/stdlib/templ.go
@@ -1,7 +1,9 @@
 package stdlib
 
 import (
+	"bytes"
 	"html/template"
+	"io"
 	"io/fs"
 	"path/filepath"
 )
@@ -22,4 +24,33 @@ func ParseTemplateDir(dir string) (*template.Template, error) {
 	}
 
 	return template.ParseFiles(paths...)
+}
+
+type Renderer struct {
+	tmpl *template.Template
+}
+
+func NewRenderer(dir string) (Renderer, error) {
+	t, err := ParseTemplateDir(dir)
+	if err != nil {
+		return Renderer{}, err
+	}
+
+	return Renderer{
+		tmpl: t,
+	}, nil
+}
+
+func (r Renderer) Render(name string, data any) (string, error) {
+	var b bytes.Buffer
+	err := r.tmpl.ExecuteTemplate(&b, name, data)
+	if err != nil {
+		return "", err
+	}
+
+	return b.String(), nil
+}
+
+func (r Renderer) RenderTo(wr io.Writer, name string, data any) error {
+	return r.tmpl.ExecuteTemplate(wr, name, data)
 }
