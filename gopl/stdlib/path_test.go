b60ab4733981df0278d67c975178ca52bd8e865a
neefrankie <neefrankie@gmail.com>
2023-12-21 20:54:30 +0800

    add TestPathGlob

--- gopl/stdlib/path_test.go
+++ gopl/stdlib/path_test.go
@@ -2,6 +2,7 @@ package stdlib
 
 import (
 	"path"
+	"path/filepath"
 	"testing"
 )
 
@@ -9,4 +10,20 @@ func TestPathBase(t *testing.T) {
 	t.Logf("%s\n", path.Base("/a/b"))
 	t.Logf("%s\n", path.Base("/"))
 	t.Logf("%s\n", path.Base(""))
+}
+
+func TestPathGlob(t *testing.T) {
+	matches, err := filepath.Glob("templates/*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("%s\n", matches)
+
+	matches, err = filepath.Glob("templates/**/*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("%s\n", matches)
 }
