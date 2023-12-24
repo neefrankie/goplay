fb0e2ced35e70711a7d7ebdb355fd1fd494d2411
neefrankie <neefrankie@gmail.com>
2023-12-24 17:43:39 +0800

    trye jwt

---
+++ web/enc/jwt_test.go
@@ -0,0 +1,48 @@
+package enc
+
+import (
+	"goplay/web/config"
+	"testing"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+func TestNewJWT(t *testing.T) {
+	key := config.MustLoad().GetJWTKey()
+	token := jwt.New(jwt.SigningMethodHS256)
+	s, err := token.SignedString([]byte(key))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Signed string: %s\n", s)
+}
+
+func TestJWTWithClaims(t *testing.T) {
+	id := uuid.New().String()
+	key := config.MustLoad().GetJWTKey()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": id,
+	})
+	s, err := token.SignedString([]byte(key))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Signed string: %s\n", s)
+
+	token, err = jwt.ParseWithClaims(s, &jwt.MapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Token.Raw: %s\n", token.Raw)
+	t.Logf("Token.Method: %v\n", token.Method)
+	t.Logf("Token.Header: %v\n", token.Header)
+	t.Logf("Token.Claims: %v\n", token.Claims)
+	t.Logf("Token.Signature: %s\n", token.Signature)
+	t.Logf("Token.Valid: %t\n", token.Valid)
+}
---
+++ web/enc/password_test.go
@@ -0,0 +1,25 @@
+package enc
+
+import (
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestEncryptPassword(t *testing.T) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte("12345678"), 14)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("%s\n", bytes)
+
+	err = bcrypt.CompareHashAndPassword(bytes, []byte("12345678"))
+	if err != nil {
+		t.Log("Password not matched")
+		t.Fatal(err)
+	} else {
+		t.Log("Password matched")
+	}
+
+}
