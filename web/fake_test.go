4f4f8b945f0ecb10320b173949a9244fee215250
neefrankie <neefrankie@gmail.com>
2023-12-26 22:22:22 +0800

    test fake

--- web/fake_test.go
+++ web/fake_test.go
@@ -1,1 +1,21 @@
 package web
+
+import (
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+)
+
+func TestFakeSimple(t *testing.T) {
+	t.Logf("Name: %s\n", gofakeit.Name())
+	t.Logf("Email: %s\n", gofakeit.Email())
+	t.Logf("Phone: %s\n", gofakeit.Phone())
+	t.Logf("BS: %s\n", gofakeit.BS())
+	t.Logf("BeerName: %s\n", gofakeit.BeerName())
+	t.Logf("Color: %s\n", gofakeit.Color())
+	t.Logf("Company: %s\n", gofakeit.Company())
+	t.Logf("CreditCardNumber: %s\n", gofakeit.CreditCard().Number)
+	t.Logf("HackerPhrase: %s\n", gofakeit.HackerPhrase())
+	t.Logf("JobTitle: %s\n", gofakeit.JobTitle())
+	t.Logf("CurrencyShort: %s\n", gofakeit.CurrencyShort())
+}
