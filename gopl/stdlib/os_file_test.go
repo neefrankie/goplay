7aa3339c3b76054c5742d81f51a1dd65cb75f57c
neefrankie <neefrankie@gmail.com>
2024-01-18 08:36:52 +0800

    doc

--- gopl/stdlib/os_file_test.go
+++ gopl/stdlib/os_file_test.go
@@ -1,10 +1,15 @@
 package stdlib
 
 import (
+	"io"
 	"os"
 	"testing"
 )
 
+// os.ReadFile read the file and returns the content as `[]byte`.
+// Under the hook it uses os.Open().
+// To manipulate files as `[]byte`, use `os.ReadFile“ and `os.WriteFile`
+// directly without toucing the File object.
 func TestReadFile(t *testing.T) {
 	data, err := os.ReadFile("./templates/index.html")
 	if err != nil {
@@ -14,19 +19,28 @@ func TestReadFile(t *testing.T) {
 	t.Logf("%s\n", data)
 }
 
+func TestWriteFile(t *testing.T) {
+	err := os.WriteFile("build/hello.txt", []byte("Hello, Gpphers"), 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestOpen(t *testing.T) {
+	f, err := os.Open("./templates/index.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	io.Copy(os.Stdout, f)
+}
+
 func TestOpenFile(t *testing.T) {
 	f, err := os.OpenFile("build/notes.txt", os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if err := f.Close(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestWriteFile(t *testing.T) {
-	err := os.WriteFile("build/hello", []byte("Hello, Gpphers"), 0666)
-	if err != nil {
 		t.Fatal(err)
 	}
 }
