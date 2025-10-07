30ac8c39ca4f6b4b14d525e1f845f32f0ac69d6f
Victor <neefrankie@gamil.com>
2025-10-08 00:37:23 +0800

    learn gorm model

---
+++ ormplay/models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID           uint
+	Name         string
+	Email        *string
+	Age          uint8
+	Birthday     *time.Time
+	MemberNumber sql.NullString
+	ActivatedAt  sql.NullString
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	ignored      string
+}
